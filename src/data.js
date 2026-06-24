64382db9f18fc51902300a03e0ff0572a2372632
muhammadrifqifuadi <rifqifuadi91@gmail.com>
2026-06-24 18:24:10 +0700

    Update Proyek

--- src/data.js
+++ src/data.js
@@ -25,6 +25,7 @@ import Tools16 from "/assets/tools/ts.png";
 import Tools17 from "/assets/tools/php.png";
 import Tools18 from "/assets/tools/vite.png";
 import Tools19 from "/assets/tools/mysql.png";
+import Tools20 from "/assets/tools/mysql.png";
 
 
 export const listTools = [
@@ -161,6 +162,13 @@ export const listTools = [
     ket: "Framework",
     dad: "1900",
   },
+  {
+    id: 20,
+    gambar: Tools20,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
 ];
 
 import Proyek1 from "/assets/proyek/proyek1.jpg";
@@ -179,7 +187,7 @@ export const listProyek = [
     fullDescription:"A mobile application powered by Artificial Intelligence and Machine Learning, developed to assist in the early detection of Adolescent Idiopathic Scoliosis (AIS). By analyzing patient data and patterns, the app provides predictions that can help doctors and patients take preventive measures. This project demonstrates the role of AI in modern healthcare by improving accuracy, accessibility, and efficiency in medical diagnosis.",
     borderColor: "#3B82F6",
     gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/RumahGitar",
     dad: "100",
   },
   {
@@ -190,7 +198,7 @@ export const listProyek = [
     fullDescription:"A smart IoT system designed to measure and analyze air quality using sensors connected to a mobile application. The app provides real-time updates on air quality levels, empowering users to make healthier lifestyle decisions based on environmental conditions. This project showcases the potential of IoT in addressing environmental and public health issues.",
     borderColor: "#10B981",
     gradient: "linear-gradient(180deg, #10B981, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/UTS_WEB",
     dad: "200",
   },
   {
@@ -201,7 +209,7 @@ export const listProyek = [
     fullDescription:"An IoT-based healthcare project developed to measure and monitor heart rate in real-time. The system connects sensors to a local web server, enabling users and healthcare providers to track data directly from a web interface. This project highlights the integration of hardware and software to create efficient medical solutions that can be applied in local clinics or personal health monitoring.",
     borderColor: "#3B82F6",
     gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/UAS_AI_Face-Expression-Zoom",
     dad: "300",
   },
   {
@@ -212,7 +220,7 @@ export const listProyek = [
     fullDescription:"An interactive web portfolio showcasing my professional journey, skills, and projects. Designed with a clean yet modern aesthetic, the portfolio highlights my technical expertise in web development, software engineering, and IoT. The site also serves as a central hub for potential employers and collaborators to explore my works, reflecting both my creativity and technical precision.",
     borderColor: "#10B981",
     gradient: "linear-gradient(180deg, #10B981, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/muhmmdrfqifuadi_",
     dad: "400",
   },
     {
@@ -223,7 +231,7 @@ export const listProyek = [
     fullDescription:"A cross-platform application available on both mobile and desktop, created to help identify different types of color blindness through color recognition tests. The app provides users with instant results, making it useful for educational, medical, and self-assessment purposes. Its intuitive design and accessibility aim to support individuals in understanding and managing color vision deficiencies.",
     borderColor: "#3B82F6",
     gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/Frontend-Kulu-Kulu-App",
     dad: "500",
   },
   {
@@ -234,7 +242,7 @@ export const listProyek = [
     fullDescription:"A fully functional website designed for a coffee shop, enabling customers to explore the menu, learn about the shop’s story, and place orders online. The project focused on creating an attractive and modern interface, integrating responsive design for mobile and desktop, and ensuring smooth navigation for users. This website not only improved customer experience but also supported the coffee shop’s digital presence and business growth.",
     borderColor: "#10B981",
     gradient: "linear-gradient(180deg, #10B981, #000)",
-    url: "https://github.com/rissss21",
+    url: "https://github.com/muhammadrifqifuadi/Capstone_Project",
     dad: "600",
   },
 ];
